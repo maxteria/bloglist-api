9ed213361d827f4ead0f3deabd0f1f3e7d4a587c
Maximiliano Morales <maxteriaok@gmail.com>
2022-11-29 12:14:50 -0300

    Fiest commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+// Connect to MongoDB
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+// Middleware
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.get('/', (request, response) => {
+  response.send(`<h1>Blog list API</h1>
+  <p>See <a href="/api/blogs">/api/blogs</a> for blog posts.</p>`)
+})
+
+app.get('/api', (request, response) => {
+  response.send(`<h1>Blog list API</h1>
+  <p>See <a href="/api/blogs">/api/blogs</a> for blog posts.</p>`)
+})
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
