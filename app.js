6d913ee5f9b7990547857c1ebc2533338a8735f3
Maximiliano Morales <maxteriaok@gmail.com>
2022-12-06 15:34:48 -0300

    ADD middleware user controller to app

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -37,6 +40,7 @@ app.get('/api', (request, response) => {
 })
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
