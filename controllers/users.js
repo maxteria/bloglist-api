8edb6ef5fb6c61b4ca8d421b67d42e2ed00814d4
Maximiliano Morales <maxteriaok@gmail.com>
2022-12-06 16:15:24 -0300

    ADD endpoint to get all users

--- controllers/users.js
+++ controllers/users.js
@@ -19,4 +19,11 @@ usersRouter.post('/', async (request, response) => {
   response.json(savedUser)
 })
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
+
+  response.json(users.map(user => user.toJSON()))
+})
+
 module.exports = usersRouter
