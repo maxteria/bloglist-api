2bda2b6f3279075e1bae3fe9d3f7f5ce8e0a4bbd
Maximiliano Morales <maxteriaok@gmail.com>
2022-12-06 18:28:00 -0300

    ADD token validation to delete some blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -46,8 +46,24 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(404).json({ error: 'blog not found' })
+  }
+
+  if (blog.user.toString() === decodedToken.id.toString()) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: 'you are not authorized to delete this blog' })
+  }
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
