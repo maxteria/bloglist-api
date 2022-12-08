4c4f90ca5975090670ae8be1d35dcbc1e3dc9052
Maximiliano Morales <maxteriaok@gmail.com>
2022-12-08 12:04:04 -0300

    ADD token validation to update endpoint

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -73,11 +73,21 @@ blogsRouter.delete('/:id', async (request, response, next) => {
 blogsRouter.put('/:id', async (request, response, next) => {
   const body = request.body
 
+  // see middleware.js for tokenExtractor
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
   const blog = {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   }
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true })
